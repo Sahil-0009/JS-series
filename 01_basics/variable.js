60d86bc34d5dcf84a8ac461467f9e48bcbf4b63f
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-12 15:11:09 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+
+const acctId = 4321000
+let acctEmail = "sahil@gmail.com"
+var acctPassword = "12345"
+acctCity = " Nagaon"
+let acctState;
+
+// acctId = 2  not allowed
+
+acctEmail = "asbd@gmail.com"
+acctPassword = " 21212"
+acctCity = "Guwahati"
+
+console.log( acctId);
+console.table([acctId, acctEmail,acctPassword,acctCity,acctState])
