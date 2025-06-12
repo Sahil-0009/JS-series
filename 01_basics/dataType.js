7758df2f241100d6522f2fd6a00491f34d64913f
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-12 15:43:18 +0000

    Variable

---
+++ 01_basics/dataType.js
@@ -0,0 +1,29 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using node , not browser
+
+console.log(3+3); 
+                    // code readability should 
+console.log("Sahil")
+
+let name = " Sahil"
+let age = 18;
+let isLoggedId = false
+
+/*
+number = 2 to power 53
+bigint
+string
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object
+*/
+
+
+console.log(typeof  "Sahil")
+console.log(typeof  null) // object
+console.log(typeof  undefined) // undefined
