f43cc49218ea26287dc6973403737b4b0f45fddc
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-13 17:51:24 +0000

    DataType Summary

---
+++ 01_basics/dataTypeSum.js
@@ -0,0 +1,36 @@
+
+// Primitive data type 
+// & types : String, Number , Boolean, null, undefined, Symbol, BigInt,
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 122112n
+
+
+
+//Reference Type (Non primitive)
+
+// Array , object , function
+
+const heros = ["Sahil", "Ram", "Arjun"]
+let myObj = {
+    name: " Rahul",
+    age: 23
+
+}
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof bigNumber)
+console.log(typeof outsideTemp)
