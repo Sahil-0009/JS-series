a1b70becc47447926296ee583f1616109059a1ed
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-13 12:10:10 +0000

    coversion of data type

---
+++ 01_basics/conversion.js
@@ -0,0 +1,34 @@
+
+let score = null
+
+console.log(typeof score)
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+
+"33" -> 33
+"33abs" -> NaN
+ture -> 1; false -> 0;
+
+
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 -> true, 0-> false
+// "" -> false
+// "Sahil" -> true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
