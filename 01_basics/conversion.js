4a377ed34e194cc341bb0eeceb25b537a0d84dd1
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-13 12:41:02 +0000

    Operation

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,13 +1,13 @@
 
 let score = null
 
-console.log(typeof score)
+// console.log(typeof score)
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 /*
 
@@ -20,7 +20,7 @@ ture -> 1; false -> 0;
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 -> true, 0-> false
 // "" -> false
@@ -30,5 +30,40 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************Operation********* 
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // power 3
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Sahil"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+ 2)
+// console.log(1+ "2")
+// console.log("1"+ 2+2)
+// console.log(1+2+ "2")
+
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
