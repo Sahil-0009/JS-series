5d07b89ecc321a3164a2dea3c242506e9c5facb3
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-13 18:27:48 +0000

    Data tyoe Summary

--- 01_basics/dataTypeSum.js
+++ 01_basics/dataTypeSum.js
@@ -32,5 +32,28 @@ const myFunction = function(){
     console.log("Hello World")
 }
 
-console.log(typeof bigNumber)
-console.log(typeof outsideTemp)
+// console.log(typeof bigNumber)
+// console.log(typeof outsideTemp)
+
+
+// =======================================================
+
+// Stack (Primitive) , Heap (Non-primitive)
+
+let myYoutubeName = "Bookreading"
+let anotherName = myYoutubeName
+anotherName ="Chai"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "sahil@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
