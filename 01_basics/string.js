72ac96396f7ee4f52a4bc47c37f2dfd08d98d800
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-14 15:14:03 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,34 @@
+
+const name = "Sah-il"
+const repoCount = 30
+
+// console.log(name + repoCount+ " Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String('Sahil')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("S"))
+
+// const newString = gameName.substring(0,3)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-5,4)
+// console.log(anotherString)
+
+// const newStringOne = "  Sahil  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = " https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace("%20","-"))
+
+console.log(url.includes('hitesh'))
+
+console.log(name.split('-'))
