614a34051e3d73b2e873c616072d047d57f7274c
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-14 19:51:46 +0000

    Date and time

---
+++ 01_basics/date.js
@@ -0,0 +1,29 @@
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.setDate())
+
+// console.log(typeof myDate)
+
+
+// let myCreateDate = new Date(2023,0,23)
+// let myCreateDate = new Date(2023,0,23,5,3)
+// let myCreateDate = new Date("2023-01-14")
+let myCreateDate = new Date("01-14-2023")
+// console.log(myCreateDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreateDate.getDate())
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getDay()+1)
+// console.log(newDate.getTime())
+
+// console.log(`${newDate.getDay()} and the time ${newDate.getTime()}`)
+// console.log(newDate.toLocaleString('default', {
+//     weekday : "long"
+// }))
