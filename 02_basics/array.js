fb73f08f67be3bd1b0f3f6dd2ea4d68f14253375
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-15 16:05:19 +0000

    Array in Javascript

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,1 +1,35 @@
+const arr = [0,1,2,3,4,5]
 
+const myHeros = ["Ram","Rohim"]
+
+const arr2 = new Array(1,2,3,4,5)
+// console.log(arr[0])
+
+// ====== Array method ========
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+// arr.unshift(9)
+// arr.shift()
+
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(3))
+
+
+const newArray = arr.join()
+
+// console.log(arr)
+// console.log(typeof newArray)
+
+// slice, splice
+console.log('A',arr)
+const myArr = arr.slice(1,3)
+
+console.log(myArr)
+console.log("B",arr)
+
+const myArr2 = arr.splice(1,3)
+console.log("C",arr)
+console.log(myArr2)
