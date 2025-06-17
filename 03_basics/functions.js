259812d74081c7229f7cc2724edca20052c036eb
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-17 14:58:17 +0000

    Functions with objects and array

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -34,4 +34,30 @@ function loginUserMessage(userName ="Sam"){
 }
 
 // console.log(loginUserMessage("Sahil"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1,val2, ...num1){// rest operator ...  sprade operator
+    return num1
+
+}
+// console.log(calculateCartPrice(200,300,400))
+
+const user = {
+    username :" Sahil",
+    price: 199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+// handleObject(user)
+handleObject({
+    username : "Sahil",
+    price : 299
+})
+
+const myArr = [200,300,400,500]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myArr))
+console.log(returnSecondValue([200,300,400]))
