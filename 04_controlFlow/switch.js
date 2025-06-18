3cddf5eb690a6264124e217239f51d5036fc4490
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-06-18 19:19:42 +0000

    switch case

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,27 @@
+
+const month = 7
+
+switch(month){
+    case 1:
+        console.log("Jan")
+        break
+    case 2:
+        console.log("feb")
+        break
+    case 3:
+        console.log("mar")
+        break
+    case 4:
+        console.log("apr")
+        break
+    case 5:
+        console.log("may")
+        break
+    case 6:
+        console.log("June")
+        break
+    default :
+        console.log("None")
+        break
+
+}
