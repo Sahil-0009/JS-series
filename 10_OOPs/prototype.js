17150c0bd81f6dbcee8afe579c243178af61c240
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-07-20 16:14:42 +0000

    prototype

---
+++ 10_OOPs/prototype.js
@@ -0,0 +1,22 @@
+// let muName = 'Sahil    '
+// let muChannel = 'Chai    '
+
+// console.log(muName.truelength);
+
+let myHeros = ['thor','spiderman']
+
+let heroPower = {
+    thor: 'hammer',
+    spiderman: 'sling',
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+        
+    }
+}
+
+Object.prototype.sahil = function(){
+    console.log(`Sahil
+    
+}
+
+heroPower.sahil()   
