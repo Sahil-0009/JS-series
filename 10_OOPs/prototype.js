afcb7ef2b798d648f7f5f9682d64cdd2856ee1bc
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-07-20 16:31:04 +0000

    prototype

--- 10_OOPs/prototype.js
+++ 10_OOPs/prototype.js
@@ -15,8 +15,55 @@ let heroPower = {
 }
 
 Object.prototype.sahil = function(){
-    console.log(`Sahil
+    console.log(`Sahil is present in all objects`)
     
 }
 
-heroPower.sahil()   
+Array.prototype.heySahil = function(){
+    console.log(`Sahil says hello`);
+    
+}
+
+// heroPower.sahil()   
+myHeros.sahil()
+myHeros.heySahil()
+// heroPower.heySahil()
+
+// inheritance 
+
+const User = {
+    name : 'Sahil',
+    email:'abc@gmail.com'
+}
+
+const Teacher = {
+    makeVedio :true
+}
+
+const TeachingSupport = {
+    isAvailable:false
+}
+
+const TASupport = {
+    makeAssignment :'JS Assignment',
+    fullTime:true,
+    __proto__: TeachingSupport
+}
+
+ Teacher.__proto__ = User
+
+ // modern syntax 
+
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUsername = "ChaiAurCode   "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`),
+    console.log(`${this.length}`),
+    console.log(`True length is : ${this.trim().length}`)
+    
+}
+anotherUsername.trueLength()
+"Sahil".trueLength()
+"iceTea".trueLength()
