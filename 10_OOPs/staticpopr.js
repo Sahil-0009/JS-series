e0f9e9aeafcd20f2e51d7748aac8346ded6118dc
Abu Jubair Md Sahil <109473654+Sahil-0009@users.noreply.github.com>
2025-07-23 21:12:58 +0000

    inheritance

---
+++ 10_OOPs/staticpopr.js
@@ -0,0 +1,27 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME : ${this.username}`);
+        
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const sahil = new User("Sahil")
+// console.log(sahil.createId())
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("Iphone","abc@gmail.com")
+console.log(iphone.logMe());
